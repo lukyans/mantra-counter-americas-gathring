48c7ae12b777cc13c63b9cc5ac40adfb5253292f
Sergey <sukyans@gmail.com>
2021-02-24 16:38:56 -0500

    update text

--- src/Components/MantrasIntro.jsx
+++ src/Components/MantrasIntro.jsx
@@ -11,17 +11,14 @@ const MantrasIntro = () => {
       {/* if you want to have an image uncomment below */}
       {/* <img src={Logo} alt="Logo" className="intro_image" /> */}
       <p className="intro_text">
-        Whether you do a formal Guru Yoga session or are doing mantras while
-        walking in the park...if you are dedicating them to{' '}
-        <a href="http://resources.diamondway.org/americas-gathering" target="_new">
-          Americas Gathering
-        </a>
-        , make that wish and simply count them and make a record of it. Then
-        input the number of mantras in the form below. Again, you do not have to
-        do a formal sitting session in order to count your Karmapa Chenno
-        mantras. Formal sessions on the 16th Karmapa in your local center are
-        recommended as well. We wish to keep this project ongoing throughout
-        this weekend.
+        Whether you are doing a formal session on the 16th Karmapa privately,
+         in your local center, or are counting mantras while walking in the park...please
+         focus your wishes as a gift to Lama Ole during this{' '}
+         <a href="http://resources.diamondway.org/americas-gathering" target="_new">
+           Americas Gathering
+         </a>.
+         Make your wish, add them up, and input the number of mantras in the form below.
+         We'll keep this mantra marathon ongoing throughout this weekend and gather as many as possible for our dear Lama!
       </p>
     </div>
   )
