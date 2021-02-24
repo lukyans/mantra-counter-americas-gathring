11072a2250a72f7532a20caccd5baf20d0055d0b
Sergey <sukyans@gmail.com>
2021-02-24 16:51:05 -0500

    update text

--- src/Components/MantrasIntro.jsx
+++ src/Components/MantrasIntro.jsx
@@ -12,13 +12,13 @@ const MantrasIntro = () => {
       {/* <img src={Logo} alt="Logo" className="intro_image" /> */}
       <p className="intro_text">
         Whether you are doing a formal session on the 16th Karmapa privately,
-         in your local center, or are counting mantras while walking in the park...please
-         focus your wishes as a gift to Lama Ole during this{' '}
-         <a href="http://resources.diamondway.org/americas-gathering" target="_new">
-           Americas Gathering
-         </a>.
-         Make your wish, add them up, and input the number of mantras in the form below.
-         We'll keep this mantra marathon ongoing throughout this weekend and gather as many as possible for our dear Lama!
+        in your local center, or are counting mantras while walking in the park...
+        please focus your wishes as a gift to Lama Ole during this{' '}
+        <a href="http://resources.diamondway.org/americas-gathering" target="_new">
+         Americas Gathering
+        </a>.
+        Make your wish, add them up, and input the number of mantras in the form below.
+        We will keep this mantra marathon ongoing throughout this weekend and gather as many as possible for our dear Lama!
       </p>
     </div>
   )
